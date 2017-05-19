62f77dde927bcfa59967052ff76c167db5d96ac7
edenjiga <edenjiga_@hotmail.com>
2017-05-19 17:39:23 -0500

    Version 1.1

--- app/src/main/java/us/example/android/fusion_tesis/UI/Ruta.java
+++ app/src/main/java/us/example/android/fusion_tesis/UI/Ruta.java
@@ -6,16 +6,19 @@ package us.example.android.fusion_tesis.UI;
 
 public class Ruta {
     private static final Ruta ourInstance = new Ruta();
-    public static final String CAMBIO_CLAVE_URL = "http://ceramicapiga.com/tesis/changePassword.php";
-    public static final String GET_SITE_INFO_URL = "http://ceramicapiga.com/tesis/get_site_info.php";
-    public static final String MAKE_RATING_URL = "http://ceramicapiga.com/tesis/makeRating.php";
-    public static final String GET_BY_TAG_URL = "http://ceramicapiga.com/tesis/get_by_tag.php";
-    public static final String GET_15_SITES_URL = "http://ceramicapiga.com/tesis/get15sites.php";
-    public static final String GET_RECOM_SITES_URL = "http://ceramicapiga.com/tesis/getRecomSites.php";
-    public static final String LOGIN_URL = "http://ceramicapiga.com/tesis/login.php";
-    public static final String BUSCAR_SITE_URL = "http://ceramicapiga.com/tesis/searchSite.php";
-    public static final String REGISTRAR_USER_URL = "http://ceramicapiga.com/tesis/registration.php";
-    public static final String GET_5_SITE_URL = "http://ceramicapiga.com/tesis/get5sites.php";
+
+    // Cada ruta tiene comentado en que actividad se es usada
+
+    public static final String CAMBIO_CLAVE_URL = "http://ceramicapiga.com/tesis/changePassword.php"; // ActividadCambioClave
+    public static final String GET_SITE_INFO_URL = "http://ceramicapiga.com/tesis/get_site_info.php";   // ActividadDetalleSitio
+    public static final String MAKE_RATING_URL = "http://ceramicapiga.com/tesis/makeRating.php";    // ActividadDetalleSitio
+    public static final String GET_BY_TAG_URL = "http://ceramicapiga.com/tesis/get_by_tag.php"; // ActividadCategoriaSeleccionada
+    public static final String GET_15_SITES_URL = "http://ceramicapiga.com/tesis/get15sites.php"; // ActividadListaCompleta
+    public static final String GET_RECOM_SITES_URL = "http://ceramicapiga.com/tesis/getRecomSites.php"; //ActividadListaCompleta
+    public static final String LOGIN_URL = "http://ceramicapiga.com/tesis/login.php"; // ActividadLogin
+    public static final String BUSCAR_SITE_URL = "http://ceramicapiga.com/tesis/searchSite.php"; //FragmentoPrincipal
+    public static final String REGISTRAR_USER_URL = "http://ceramicapiga.com/tesis/registration.php"; //ActividadRegistro
+    public static final String GET_5_SITE_URL = "http://ceramicapiga.com/tesis/get5sites.php"; //ActividadPrincipal
 
 
     public static Ruta getInstance() {
